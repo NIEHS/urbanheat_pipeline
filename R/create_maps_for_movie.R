f3e0fbe0238e527ed2cf990b13d24e4acccd4dcd
Eva Marques <marquesel@gn040801.hpc.niehs.nih.gov>
2025-05-23 12:07:59 -0400

    update uhi maps to add main roads shapefile

--- R/create_maps_for_movie.R
+++ R/create_maps_for_movie.R
@@ -67,12 +67,19 @@ save_maps_uhi <- function(temp, shp, tz, ts, te, storage_path, uhi_range) {
   temp_avg$time <- terra::time(temp)
   uhi <- temp - terra::global(temp, "mean", na.rm = TRUE)$mean
   period <- terra::time(temp)
-  if (shp == "") {
+  if (is(shp, "SpatVector")) {
     for (p in period) {
       cat(p, "\n")
       p_str <- strftime(p, format = "%Y-%m-%d %H:%M:%S", tz = tz) |>
         as.POSIXct(tz = tz)
-      plot <- map_uhi(uhi, p_str, uhi_range)
+      plot <- map_uhi(uhi, p_str, uhi_range) +
+        tidyterra::geom_spatvector(
+          data = shp,
+          fill = NA,
+          size = 1,
+          alpha = 1,
+          linewidth = .1
+        )
       ggplot2::ggsave(plot,
         filename = paste0(
           storage_path,
@@ -90,13 +97,7 @@ save_maps_uhi <- function(temp, shp, tz, ts, te, storage_path, uhi_range) {
       cat(p, "\n")
       p_str <- strftime(p, format = "%Y-%m-%d %H:%M:%S", tz = tz) |>
         as.POSIXct(tz = tz)
-      plot <- map_uhi(uhi, p_str, uhi_range) +
-        tidyterra::geom_spatvector(
-          data = shp,
-          fill = NA,
-          size = 2,
-          linewidth = .1
-        )
+      plot <- map_uhi(uhi, p_str, uhi_range)
       ggplot2::ggsave(plot,
         filename = paste0(
           storage_path,
@@ -112,15 +113,76 @@ save_maps_uhi <- function(temp, shp, tz, ts, te, storage_path, uhi_range) {
   }
 }
 
+round_to_upper_even <- function(x) {
+  if (ceiling(x) %% 2 == 0) {
+    return(ceiling(x))
+  } else {
+    return(ceiling(x) + 1)
+  }
+}
+
+map_uhi_avg <- function(uhi_avg) {
+  temp_ipcc <- list(
+    c(103, 0, 31),
+    c(178, 24, 43),
+    c(214, 96, 77),
+    c(244, 165, 130),
+    c(253, 219, 199),
+    c(247, 247, 247),
+    c(209, 229, 240),
+    c(146, 197, 222),
+    c(67, 147, 195),
+    c(33, 102, 172),
+    c(5, 48, 97)
+  ) |>
+    lapply(
+      function(x) grDevices::rgb(x[1], x[2], x[3], maxColorValue = 255)
+    ) |>
+    rev()
+  uhi_range <- round_to_upper_even(
+    terra::minmax(uhi_avg)[2] - terra::minmax(uhi_avg)[1]
+  )
+  p <- ggplot2::ggplot() +
+    tidyterra::geom_spatraster(data = uhi_avg) +
+    ggplot2::labs(
+      fill = latex2exp::TeX("$UHI$(°C)")
+    ) +
+    ggplot2::scale_fill_gradientn(
+      colours = temp_ipcc,
+      na.value = NA,
+      limits = c(-uhi_range / 2, uhi_range / 2),
+      breaks = seq(-uhi_range / 2, uhi_range / 2, by = 1)
+    ) +
+    ggplot2::guides(
+      fill = ggplot2::guide_colourbar(barwidth = 25, barheight = 1.5)
+    ) +
+    ggspatial::annotation_scale(
+      location = "tl", pad_x = ggplot2::unit(1, "cm"),
+      pad_y = ggplot2::unit(1, "cm"),
+      height = ggplot2::unit(0.30, "cm"),
+      text_cex = 1
+    ) +
+    ggspatial::annotation_north_arrow(
+      location = "br",
+      which_north = "true",
+      pad_x = ggplot2::unit(0.2, "cm"),
+      pad_y = ggplot2::unit(0.2, "cm")
+    ) +
+    ggplot2::theme(
+      title = ggplot2::element_text(size = 18),
+      axis.text = ggplot2::element_text(size = 12),
+      axis.text.y = ggplot2::element_text(size = 12, angle = 90, hjust = .5),
+      plot.caption = ggplot2::element_text(size = 10),
+      legend.position = "bottom",
+      legend.text = ggplot2::element_text(size = 18),
+      legend.title = ggplot2::element_text(size = 18),
+      panel.background = ggplot2::element_rect(fill = "white"),
+      panel.grid.major = ggplot2::element_line(colour = "grey")
+    )
+  p
+}
+
 args <- commandArgs(trailingOnly = TRUE)
-# args test in interactive mode
-# args <- NULL
-# args[1] <- "./rasters/albuquerque_2020100100_2020103123/inference_predmean_albuquerque_2020100100_2020103123.tif"
-# args[2] <- ""
-# args[3] <- "UTC"
-# args[4] <- "2020-10-01 00:00:00"
-# args[5] <- "2020-10-31 23:00:00"
-# args[6] <- "test"
 
 cat(args, "\n")
 # code to find LST (does not work everywhere for some unclear reason)
@@ -134,6 +196,9 @@ cat(args, "\n")
 if (!dir.exists(args[6])) {
   dir.create(args[6], recursive = TRUE)
 }
+if (!dir.exists("my_uhis")) {
+  dir.create("my_uhis", recursive = TRUE)
+}
 
 temp <- terra::rast(args[1])
 cat("temp loaded \n")
@@ -144,14 +209,17 @@ ts_all <- as.POSIXct(
 )
 te_all <- ts_all + lubridate::hours(dim(temp)[3]) - 1
 terra::time(temp) <- seq(ts_all, te_all, by = "hour")
+if (args[2] == "") {
+  shape <- args[2]
+} else {
+  shape <- terra::vect(args[2])
+  shape <- terra::crop(shape, terra::ext(temp)) # Crop shape to boundaries of temp
+}
+cat("shape loaded\n")
 
 save_maps_uhi(
   temp,
-  shp = ifelse(
-    args[2] == "",
-    args[2],
-    terra::vect(args[2])
-  ),
+  shp = shape,
   tz = args[3],
   ts = ts_all,
   te = te_all,
@@ -159,3 +227,19 @@ save_maps_uhi(
   uhi_range = as.numeric(args[7])
 )
 cat("images created \n")
+
+uhi <- temp - terra::global(temp, "mean", na.rm = TRUE)$mean
+uhi_avg <- terra::mean(uhi)
+if (is(shape, "SpatVector")) {
+  p <- map_uhi_avg(uhi_avg) +
+    tidyterra::geom_spatvector(
+      data = shape,
+      fill = NA,
+      size = 1,
+      alpha = 1,
+      linewidth = .1
+    )
+} else {
+  p <- map_uhi_avg(uhi_avg)
+}
+ggplot2::ggsave(p, file = paste0("my_uhis/uhi_avg_", args[8], ".png"))
--- create_movie.sh
+++ create_movie.sh
@@ -6,18 +6,17 @@
 #SBATCH --output=slurm_messages_movies/slurm-%j.out
 #SBATCH --error=slurm_messages_movies/slurm-%j.err
 
-RASTER_FILE="./output/winston-salem_2023070100_2023073123/inference_predmean_winston-salem_2023070100_2023073123.tif"
-SHAPE_FILE=""
+RASTER_FILE="./output/san_francisco_2023070100_2023073123/inference_predmean_san_francisco_2023070100_2023073123.tif"
+SHAPE_FILE="./input/shapes/NTAD_North_American_Roads_-6941702301048783378/North_American_Roads.shp"
 TIMEZONE="UTC"
 START_DATE="2023-07-01 00:00:00"
 END_DATE="2023-07-31 23:00:00"
 OUTPUT_DIR="my_movies"
-OUTPUT_FILENAME="winston-salem_202307.mp4"
-UHI_RANGE=10
+OUTPUT_FILENAME="san_francisco_202307"
+UHI_RANGE=20
 
 apptainer exec container/container_movies.sif bash -c "
-  Rscript R/find_lst_zone.R &&
-  Rscript R/create_maps_for_movie.R \"$RASTER_FILE\" \"$SHAPE_FILE\" \"$TIMEZONE\" \"$START_DATE\" \"$END_DATE\" \"$OUTPUT_DIR\" \"$UHI_RANGE\" &&
-  ffmpeg -y -framerate 10 -i "$OUTPUT_DIR"/%d.png -c:v libx264 -pix_fmt yuv420p "$OUTPUT_DIR"/"$OUTPUT_FILENAME"
-  rm "$OUTPUT_DIR"/*.png
+  Rscript R/create_maps_for_movie.R \"$RASTER_FILE\" \"$SHAPE_FILE\" \"$TIMEZONE\" \"$START_DATE\" \"$END_DATE\" \"$OUTPUT_DIR\"/temporary_\"$OUTPUT_FILENAME\" \"$UHI_RANGE\" \"$OUTPUT_FILENAME\" &&
+  ffmpeg -y -framerate 10 -i "$OUTPUT_DIR"/temporary_"$OUTPUT_FILENAME"/%d.png -c:v libx264 -pix_fmt yuv420p "$OUTPUT_DIR"/"$OUTPUT_FILENAME".mp4
+  rm -r "$OUTPUT_DIR"/temporary_"$OUTPUT_FILENAME"
 "
