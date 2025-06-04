c65a13a91c41c4725a012af69e5dc7a08a1bee59
Eva Marques <marquesel@gn040801.hpc.niehs.nih.gov>
2025-06-04 13:53:34 -0400

    path pb

---
+++ inst/config/path_config_fake.sh
@@ -0,0 +1,3 @@
+
+# Define the absolute path
+export WU_IBM_PATH=<absolut_path_to_WU_IBM>:/WU_IBM
--- inst/scripts/run_data_creation.sh
+++ inst/scripts/run_data_creation.sh
@@ -2,14 +2,16 @@
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=30
-#SBATCH --partition=geo
+#SBATCH --partition=highmem
 #SBATCH --output=slurm_messages/slurm-%j.out
 #SBATCH --error=slurm_messages/slurm-%j.err
 
 
 export CONTAINER=samba
 
-#apptainer shell container/container_samba.sif
+# Load the absolute path from an external file
+source inst/config/path_config.sh
+
 apptainer exec \
-  --bind /ddn/gs1/group/set/WU_IBM:/WU_IBM \
+  --bind $WU_IBM_PATH \
   container/container_samba.sif Rscript inst/targets/target_run.R
